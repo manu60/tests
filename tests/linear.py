6bc2a8d19a9f9c3b25ab46e00253f17fde3b70e9
manman <oliveira.manuel1@neuf.fr>
2020-04-30 10:51:33 +0200

    cinq

--- tests/linear.py
+++ tests/linear.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 # On charge le dataset
 house_data = pd.read_csv('house.csv')
 
+house_data = house_data[house_data['loyer'] < 10000]
+
 # On affiche le nuage de points dont on dispose
 plt.plot(house_data['surface'], house_data['loyer'], 'ro', markersize=4)
+# On affiche la droite entre 0 et 250
+plt.plot([0,250], [theta.item(0),theta.item(0) + 250 * theta.item(1)], linestyle='--', c='#000000')
 plt.show()
